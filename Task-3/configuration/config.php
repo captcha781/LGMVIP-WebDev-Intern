013a0dad355da2d8d0560d242e2d7d431ad1c064
captcha781 <bhuvanesh19112001@gmail.com>
2022-01-12 20:51:35 +0530

    Final Commit

--- Task-3/configuration/config.php
+++ Task-3/configuration/config.php
@@ -5,6 +5,10 @@ $user = "root";
 $pass = "root";
 $database = "school";
 
+/*
+Change the above settings according to your phpmyadmin and mysql configurations
+*/
+
 
 $conn = mysqli_connect($server, $user, $pass, $database);
 if (!$conn) {
